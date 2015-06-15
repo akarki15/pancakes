f70a0f56c4c18fe4620eb713f232832febf36ad7
Aashish Karki <0133asis@gmail.com>
2015-06-15 11:10:52 -0400

    Documentation, Cleanup

--- Assets/Scripts/Main/GamePlay.cs
+++ Assets/Scripts/Main/GamePlay.cs
@@ -2,16 +2,19 @@
 using System.Collections;
 
 public class GamePlay : MonoBehaviour {
+	// GamePlay stores the type of the game, initializes the pancake objects, and handles animation and game-objective-checks
 
-	// Use this for initialization
+	// upperlimit stores the maximum size of pancake possible, lowerLimit stores the minimum
 	public int upperLimit;
 	public int lowerLimit;
 	public int size;
-	// pancakes stores size, cakes stores gameobject
-	public GameObject[]cakes;
+	// size stores the number of pancakes, cakes[] stores the pancake objects
+	public GameObject[] cakes;
 	private GameObject userSettings;
-
+	// won stores if the game has been won or not
 	private bool won;
+
+	// Init
 	void Start () {
 		userSettings = GameObject.FindGameObjectsWithTag ("userSettings")[0];
 		size = userSettings.GetComponent<userSettings>().size;
@@ -26,7 +29,7 @@ public class GamePlay : MonoBehaviour {
 	public GameObject prefab;
 	public GameObject anim;
 
-	// Create the pancakes and give them size
+	// Create the pancakes and give a random length betweeen [lowerLimti, upperLimit]
 	void draw(){
 		for (int i=0;i<size;i++){
 			Vector3 pos=new Vector3(0,-i*2F,0);
@@ -38,7 +41,7 @@ public class GamePlay : MonoBehaviour {
 	}
 
 
-	// Handles Animations for flip	
+	// Handle flip Animation for pancakes when a pancake indexed at i in cakes[] is touched
 	public void flip(int i){
 		if (!won) { // Don't want the pancakes to animate when the user taps them after having won
 				for (int counter =0; counter<=i; counter++) {
@@ -56,21 +59,23 @@ public class GamePlay : MonoBehaviour {
 				StartCoroutine(wait(0.7F));					
 			}
 		}
-
 	}
 
+	// Checks whether the pancakes are arranged in ascending order
 	bool check(){
 		bool w = true;
 		for (int counter =1; counter<size; counter++)
 						w = w && (cakes [counter].GetComponent<PanProperties> ().size >= cakes [counter - 1].GetComponent<PanProperties> ().size);
 		return w;
 	}
-	//swap to keep cakes[] up to date
+
+	// Update cakes[] when a pancake is touched
 	void swap(int i, int j){
 		GameObject temp = cakes [i];
 		cakes [i] = cakes [j];
 		cakes [j] = temp;
 	}
+
 	// This coroutine lets the animation finish before changing the scene 
 	IEnumerator wait(float waitTime){
 		yield return new WaitForSeconds (waitTime);
